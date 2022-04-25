0b3330bdf8a3e907044ea929a2e7f65273058b3e
Alexander Tarasov <tarassov.al@gmail.com>
2022-04-26 02:38:18 +0300

    feat: adds redux-first-history

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,19 +1,20 @@
 import { Provider } from "react-redux";
-import store from "../../services/store/store";
+import { store, history } from "../../services/store/store";
 import styles from "./app.module.css";
 
 import MainLayout from "../main-layout/main-layout";
-import { BrowserRouter } from "react-router-dom";
+
+import { HistoryRouter as Router } from "redux-first-history/rr6";
 import AppHeader from "../app-header/app-header";
 
 function App() {
 	return (
 		<div className={styles.app}>
 			<Provider store={store}>
-				<BrowserRouter>
+				<Router history={history}>
 					<AppHeader />
 					<MainLayout />
-				</BrowserRouter>
+				</Router>
 			</Provider>
 		</div>
 	);
--- src/services/store/store.js
+++ src/services/store/store.js
@@ -5,15 +5,22 @@ import ingredientReducers from "../reducers/ingredients-reducers";
 import orderReducers from "../reducers/order-reducers";
 import systemReducers from "../reducers/system-reducers";
 import userReducers from "../reducers/user-reducers";
+import { createReduxHistoryContext } from "redux-first-history";
+import { createBrowserHistory } from "history";
 
-const store = configureStore({
+const { createReduxHistory, routerMiddleware, routerReducer } =
+	createReduxHistoryContext({ history: createBrowserHistory() });
+
+export const store = configureStore({
 	reducer: {
+		router: routerReducer,
 		ingredients: ingredientReducers,
 		elements: elementsReducers,
 		order: orderReducers,
 		system: systemReducers,
 		user: userReducers,
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(routerMiddleware),
 });
-
-export default store;
+export const history = createReduxHistory(store);
